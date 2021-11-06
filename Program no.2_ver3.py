e3d3614f57b902a26d7e64942a9b7ea6664f9c63
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-06 19:24:41 +0800

    Pseudo Code and Input Function

--- Program no.2_ver3.py
+++ Program no.2_ver3.py
@@ -4,15 +4,22 @@
 # The total amount is ______.
 
 def getAmountAppleOrange():
-    apples_ = int(input("How many apple do you want to buy?: "))
-    oranges_ = int(input("How many oranges do you want to buy?: "))
-    return apples_, oranges_
+    applesA = int(input('How many apple do you want to buy?: '))
+    orangesA = int(input('How many oranges do you want to buy?: '))
+    return applesA, orangesA
 
-
+def calculateFormula(applesB,orangesB):
+    return applesB * 20 + orangesB * 25
     
+    
+def display(apples_, oranges_,totalPrice_):
+    print(f'The total amount for {apples_} apples and {oranges_} oranges is {totalPrice_}.')
+
+
 # Steps
 # Ask how many apples and oranges you want to buy?
 apples, oranges = getAmountAppleOrange()
 #Calculate
-
+totalPrice = calculateFormula(apples,oranges)
 # Display
+display(apples, oranges, totalPrice)
---
+++ Program no.3_ver3.py
@@ -0,0 +1,18 @@
+# Create a program which you will enter the amount of money you have, it will also ask for the price of an apple.
+# Display the maximum number of apples that you can buy and the remaining money that you will have.
+# Display the output in the following format.
+# You can buy ___ apples and your change is ___ pesos.
+
+def priceTotal():
+    money_ = int(input("How much money do you have?: "))
+    priceA = int(input("How much is an apple?: "))
+    priceO = int(input("How much is an orange?: "))
+    return money_, priceA, priceO
+
+
+# Steps
+#1 Input amount of money and price of apple
+
+#3 Formula for Total amount of apples and Change
+
+#5 Display
