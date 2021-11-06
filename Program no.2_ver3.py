2d55e7e4eaf2ebc5149e850c1cb7a7c87d681129
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-06 18:13:32 +0800

    Input Function

---
+++ Program no.2_ver3.py
@@ -0,0 +1,15 @@
+# Create a program that will ask how many apples and oranges you want to buy.
+# Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
+# Display the output in the following format.
+# The total amount is ______.
+
+def getAmountAppleOrange():
+    apples_ = int(input("How many apple do you want to buy?: "))
+    oranges_ = int(input("How many oranges do you want to buy?: "))
+
+# Steps
+# Ask how many apples and oranges you want to buy?
+
+#Calculate
+
+# Display
